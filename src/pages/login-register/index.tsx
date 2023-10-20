344fc594ad7e6bde8530affb389ee8462793ebee
RusdyZain <ryanfajri28@gmail.com>
2023-10-20 15:23:36 +0800

    Progress Flex

--- src/pages/login-register/index.tsx
+++ src/pages/login-register/index.tsx
@@ -1,1 +1,14 @@
 import React from 'react';
+
+export default function Login() {
+    return (
+        <div className="flex flex-row">
+            <div className="bg-[#210F66] basis-1/2 ">
+                Ini Gambar
+            </div>
+            <div className="basis-1/2">
+                Ini Form
+            </div>
+        </div>
+    );
+};
