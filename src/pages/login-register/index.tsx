7f8a65d89da084d38f013ca9031434a287a6824c
RusdyZain <ryanfajri28@gmail.com>
2023-10-20 15:30:01 +0800

    Test

--- src/pages/login-register/index.tsx
+++ src/pages/login-register/index.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 export default function Login() {
     return (
-        <div className="flex flex-row">
-            <div className="bg-[#210F66] basis-1/2 ">
-                Ini Gambar
-            </div>
-            <div className="basis-1/2">
-                Ini Form
-            </div>
+        <div>
         </div>
     );
 };
