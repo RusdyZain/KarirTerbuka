3c2507a27ddf5e7ea6ef8e18c7f1b259008e5350
RusdyZain <ryanfajri28@gmail.com>
2023-10-22 01:01:00 +0800

    Image Login

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "watusampu.palukota.go.id",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/login-register/index.tsx
+++ src/pages/login-register/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Image from 'next/image';
 
 export default function Login() {
     return (
         <div className='flex flex-row'>
-            <div className='bg-black basis-1/2 text-white'>Tes</div>
-            <div className='basis-1/2'>Tes</div>
+            <div className='basis-1/2 relative'>
+                <div className="bg-[#210F66] absolute top-0 left-0 w-full h-full z-10 opacity-60">
+                </div>
+                <Image src="https://watusampu.palukota.go.id/wp-content/uploads/2022/10/team6.jpg" alt="Background Login" layout="responsive" width={0} height={0} className='z-0' />
+            </div>
+
+            <div className='basis-1/2'></div>
         </div>
     );
 };
