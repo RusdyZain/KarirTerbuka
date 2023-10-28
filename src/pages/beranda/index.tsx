9c97e1693ee540f5c131bfc947f2f3431029d584
laludonisetiawan <laludonisetiawan@gmail.com>
2023-10-29 04:21:31 +0800

    update navigation Link menjadi component

--- src/pages/beranda/index.tsx
+++ src/pages/beranda/index.tsx
@@ -1,21 +1,12 @@
-
-import Link from 'next/link';
-import Image from 'next/image';
+import Header from "@/components/Header";
 const Beranda = () => {
-return (
-  <div>
-   <header className='transparent absolute top-0 left-0 w-full flex items-center z-10 '>
-<div className="container">
-  <div className="flex items-center justify-between relative pl-[102px]">
-    <div className="px-4 flex ">
-    <Image src="/logo_rafflesia.svg" alt="Logo Rafflesia" width={54} height={44} />
-    <h1 className='text-2xl font-bold text-blue-600 pt-2 '>Karir Terbuka</h1>
+  return (
+    <div>
+      <div className="container mx-auto">
+       <Header/>
+      </div>
     </div>
-  </div>
-</div>
-   </header>
-  </div>
-)
-}
+  );
+};
 
 export default Beranda;
