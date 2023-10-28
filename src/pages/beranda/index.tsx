af2aa542bccd994d86c98979a485e0f9d594f95d
laludonisetiawan <laludonisetiawan@gmail.com>
2023-10-28 19:38:54 +0800

    setup font-family lato & test logo navbar

--- src/pages/beranda/index.tsx
+++ src/pages/beranda/index.tsx
@@ -1,5 +1,21 @@
+
+import Link from 'next/link';
+import Image from 'next/image';
 const Beranda = () => {
-  return <>ini halaman beranda</>;
-};
+return (
+  <div>
+   <header className='transparent absolute top-0 left-0 w-full flex items-center z-10 '>
+<div className="container">
+  <div className="flex items-center justify-between relative pl-[102px]">
+    <div className="px-4 flex ">
+    <Image src="/logo_rafflesia.svg" alt="Logo Rafflesia" width={54} height={44} />
+    <h1 className='text-2xl font-bold text-blue-600 pt-2 '>Karir Terbuka</h1>
+    </div>
+  </div>
+</div>
+   </header>
+  </div>
+)
+}
 
 export default Beranda;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        lato: ['Lato', 'sans'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
