bc3a7cc722ed98363853ee1d328f16adf620d03e
laludonisetiawan <laludonisetiawan@gmail.com>
2023-10-29 23:19:14 +0800

    refactoring ke-2 hero section

--- src/pages/beranda/index.tsx
+++ src/pages/beranda/index.tsx
@@ -1,43 +1,89 @@
 import Header from '@/components/Header';
 import Image from 'next/image';
+
 const Beranda = () => {
   return (
-    <div>
-      <div className="container mx-auto font-lato">
+    <div className='font-lato'>
+      <div className="container mx-auto">
         <Header />
 
-        <section className="pl-[102px] relative ">
-          <div className="flex w-8/12  pt-[86px]">
+        <section className="flex pl-[102px] relative">
+          <div className="flex w-8/12 pt-[86px] relative">
             <div>
-              <p className="font-bold text-7xl py-5 ">
+              <p className="font-bold text-7xl py-5">
                 Cara Mudah Mendapatkan{' '}
                 <span className="text-blue-600">Pekerjaan Impian </span>Anda{' '}
               </p>
-              <p className="text-2xl w-[602.14px] p-x-2.5 p-y-2.5	">
+              <p className="text-2xl w-[602.14px] p-x-2.5 p-y-2.5">
                 Memberikan kemudahan untuk masyarakat penyandang disabilitas
                 dalam mencari lowongan pekerjaan
               </p>
+              <div className="pt-11 pb-[362.91px]">
+                <button
+                  type="button"
+                  className="bg-blue-600 text-white text-2xl font-semibold font-lato p-2 py-3.5 px-8 w-[267px] rounded-xl"
+                >
+                  Daftar Kerja
+                </button>
+              </div>
             </div>
           </div>
-          <div className="sm:hidden md-hidden lg:block absolute w-12/12 h-screen right-0 top-40 ">
+
+          {/* awal gambar */}
+          <div className="absolute h-screen right-0 top-40 -z-20">
             <section className=" ">
               <Image
                 src="/Ellipse-3.svg"
-                alt="Logo Rafflesia"
-                width={652.44}
-                height={657.9}
-                className='-z-1'
+                alt="ellipse"
+                width={652}
+                height={657}
+                className="-z-10"
+              />
+              <Image
+                src="/EllipseOne.svg"
+                alt="ellipse"
+                width={225.9}
+                height={215.66}
+                className="absolute top-0 right-[558.25px] pt-[204.05px] -z-5"
               />
               <Image
                 src="/Ellipse-2.svg"
-                alt="Logo Rafflesia"
+                alt="ellipse"
                 width={464.76}
                 height={468.17}
-                className='absolute top-0 right-[420px] pt-[267px] -z-10'
+                className="absolute top-0 right-[420px] pt-[267px] -z-10"
               />
-            </section>-pl-[200px]
+              <Image
+                src="/hero-img-1.png"
+                alt="ellipse"
+                width={440.87}
+                height={657.9}
+                className="absolute top-0 right-[270.05px] "
+              />
+            </section>
           </div>
+          {/* akhir gambar */}
         </section>
+
+        {/* awal quotes */}
+        <div className="container bg-blue-800 max-w-full pb-0 h-[252.51px] text-2xl font-lato font-semibold text-center py-11  px-40">
+          <p className="  text-white pt-2.5">
+            “Ketika saya bertemu orang-orang sukses saya bertanya sekitar 100
+            pertanyaan untuk mengetahui atribut keberhasilan mereka. Hal ini
+            biasanya sama:{' '}
+            <span className=" font-semibold text-yellow-500">kegigihan</span>,
+            <span className=" font-semibold text-yellow-500"> kerja keras</span>{' '}
+            dan{' '}
+            <span className=" font-semibold text-yellow-500">
+              mempekerjakan orang baik
+            </span>
+            ”
+          </p>
+          <p className=" font-semibold text-yellow-500 pt-2.5 pb-[45.47px]">
+            __ Kiana Tom __
+          </p>
+        </div>
+        {/* akhir quotes */}
       </div>
     </div>
   );
