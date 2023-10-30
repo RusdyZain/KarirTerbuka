206fe25ddb83e92eb461f50f4588a8996127bed8
RusdyZain <ryanfajri28@gmail.com>
2023-10-30 11:54:41 +0800

    Daftar Done

--- src/components/daftar/FormDaftar.tsx
+++ src/components/daftar/FormDaftar.tsx
@@ -4,46 +4,52 @@ import React, { useState } from 'react';
 
 export default function FormDaftar() {
     const [selectedDate, setSelectedDate] = useState(null);
+
     return (
         <div className="mt-6 w-9/12">
-            <div className="mt-6">
-                <h2 className="text-lg font-semibold">Nama Lengkap</h2>
-                <input type="text" name="nama_lengkap" className="mt-2 bg-gray-50 border border-[#526077] sm:text-sm rounded-lg block w-full p-2.5" placeholder="Tulis Nama Lengkap" required={true} />
+            {renderInput("Nama Lengkap", "text", "nama_lengkap", "Tulis Nama Lengkap")}
+            {renderInput("Email Pengguna", "email", "email_pendaftar", "Tulis Email Anda")}
+            {renderInput("Sandi", "password", "sandi_pendaftar", "Tulis Sandi Anda")}
+            {renderInput("Masukkan Ulang Sandi", "password", "sandi_konfirmasi", "Tuliskan Ulang Sandi")}
+
+            <div className="mt-6 grid grid-cols-2 gap-2">
+                {renderDateInput("Tanggal Lahir")}
+                {renderInput("Nomor Hp/WA", "tel", "number_wa", "Nomor Telepon")}
             </div>
-            <div className="mt-6">
-                <h2 className="text-lg font-semibold">Email Pengguna</h2>
-                <input type="email" name="email_pendaftar" className="mt-2 bg-gray-50 border border-[#526077] sm:text-sm rounded-lg block w-full p-2.5" placeholder="Tulis Email Anda" required={true} />
-            </div>
-            <div className="mt-6">
-                <h2 className="text-lg font-semibold">Sandi</h2>
-                <input type="password" name="sandi_pendaftar" className="mt-2 bg-gray-50 border border-[#526077] sm:text-sm rounded-lg block w-full p-2.5" placeholder="Tulis Sandi Anda" required={true} />
-            </div>
-            <div className="mt-6">
-                <h2 className="text-lg font-semibold">Masukkan Ulang Sandi</h2>
-                <input type="password" name="sandi_konfirmasi" className="mt-2 bg-gray-50 border border-[#526077] sm:text-sm rounded-lg block w-full p-2.5" placeholder="Tuliskan Ulang Sandi" required={true} />
-            </div>
-            <div className="mt-6 flex flex-row gap-2">
-                <div className="basis-1/2">
-                    <h2 className="text-lg font-semibold">Tanggal Lahir</h2>
-                    <div className="mt-2 bg-gray-50 border border-[#526077] sm:text-sm rounded-lg w-full p-2.5 flex flex-row gap-2">
-                        <svg className=" basis-1/8 w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                        </svg>
-                        <DatePicker
-                            selected={selectedDate}
-                            onChange={(date) => setSelectedDate(date)}
-                            dateFormat="MM/dd/yyyy"
-                            placeholderText="Select date"
-                        />
-                    </div>
-                </div>
-                <div className="basis-1/2">
-                    <h2 className="text-lg font-semibold">Nomor Hp/WA</h2>
-                    <div>
-                        <input name="number_wa" className="mt-2 bg-gray-50 border border-[#526077] sm:text-sm w-full rounded-lg p-2.5" placeholder="Nomor Telepon" required={true} />
-                    </div>
-                </div>
-            </div>
-        </div >
+        </div>
     );
-};
+
+    function renderInput(label: string | number | undefined, type: string | undefined, name: string | undefined, placeholder: string | undefined) {
+        return (
+            <div className="mt-6">
+                <h2 className="text-lg font-semibold">{label}</h2>
+                <input
+                    type={type}
+                    name={name}
+                    className="mt-2 bg-gray-50 border border-[#526077] sm:text-sm rounded-lg block w-full p-2.5"
+                    placeholder={placeholder}
+                    required
+                />
+            </div>
+        );
+    }
+
+    function renderDateInput(label: string | number) {
+        return (
+            <div className="mt-6">
+                <h2 className="text-lg font-semibold">{label}</h2>
+                <div className="mt-2 bg-gray-50 border border-[#526077] sm:text-sm rounded-lg w-full p-2.5 flex flex-row gap-2">
+                    <svg className="basis-1/8 w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                    </svg>
+                    <DatePicker
+                        selected={selectedDate}
+                        onChange={(date) => setSelectedDate(date)}
+                        dateFormat="MM/dd/yyyy"
+                        placeholderText="Select date"
+                    />
+                </div>
+            </div>
+        );
+    }
+}
--- src/components/daftar/FormDaftarLanjutan.tsx
+++ src/components/daftar/FormDaftarLanjutan.tsx
@@ -1,79 +1,152 @@
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
+type DropdownProps = {
+    label: string;
+    options: string[];
+    selectedValue: string;
+    onValueChange: (value: string) => void;
+    isOpen: boolean;
+    onToggle: () => void;
+};
+
+const Dropdown = ({ label, options, selectedValue, onValueChange, isOpen, onToggle }: DropdownProps) => (
+    <div className="dropdown mt-2">
+        <label
+            tabIndex={0}
+            className="justify-start w-[20rem] btn text-sm font-semibold bg-white border-[#2570EB] hover:bg-[#2570EB] hover:bg-opacity-20"
+            onClick={onToggle}
+        >
+            <FontAwesomeIcon icon={isOpen ? faAngleRight : faAngleDown} style={{ marginLeft: '8px' }} />
+            {selectedValue}
+        </label>
+        <ul
+            tabIndex={0}
+            className={`dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-[20rem] ${isOpen ? 'open' : ''}`}
+        >
+            {options.map((option) => (
+                <li key={option} onClick={() => onValueChange(option)}><a>{option}</a></li>
+            ))}
+        </ul>
+    </div>
+);
+
 export default function FormDaftar() {
+    const genderOptions = ["Laki-Laki", "Perempuan"];
+    const educationOptions = ["Tidak Sekolah", "Sekolah Dasar (SD)", "Sekolah Menengah Pertama (SMP)", "Sekolah Menengah Atas (SMA)", "Diploma (D1, D2, D3, D4)", "Sarjana (S1)", "Magister (S2)", "Doctor (S3)"];
+    const activityOptions = ["Olahraga", "Acara Sosial", "Liburan", "Bermain Game", "Komunitas"];
+    const cityOptions = ["Mataram", "Lombok Barat", "Lombok Tengah", "Lombok Timur", "Lombok Utara", "Kabupaten Bima", "Kota Bima", "Dompu", "Sumbawa", "Sumbawa Barat"];
+
     const [selectedGender, setSelectedGender] = useState("L/P");
     const [selectedEducation, setSelectedEducation] = useState("Pendidikan");
+    const [selectedActivity, setSelectedActivity] = useState("Minat");
+    const [selectedCity, setSelectedCity] = useState("Kabupaten");
+    const [selectedSubcity, setSelectedSubcity] = useState("Kecamatan");
 
     const [isGenderDropdownOpen, setIsGenderDropdownOpen] = useState(false);
     const [isEducationDropdownOpen, setIsEducationDropdownOpen] = useState(false);
-
-    const handleGenderChange = (gender) => {
-        setSelectedGender(gender);
-        setIsGenderDropdownOpen(false);
-    };
-
-    const handleEducationChange = (education) => {
-        setSelectedEducation(education);
-        setIsEducationDropdownOpen(false);
-    };
-
-    const toggleGenderDropdown = () => {
-        setIsGenderDropdownOpen(!isGenderDropdownOpen);
-    };
-
-    const toggleEducationDropdown = () => {
-        setIsEducationDropdownOpen(!isEducationDropdownOpen);
-    };
+    const [isActivityDropdownOpen, setIsActivityDropdownOpen] = useState(false);
+    const [isCityDropdownOpen, setIsCityDropdownOpen] = useState(false);
+    const [isSubcityDropdownOpen, setIsSubcityDropdownOpen] = useState(false);
 
     return (
-        <div className="mt-6 w-9/12">
-            <div className="mt-6 flex flex-row gap-2">
-                <div className="basis-1/2">
+        <div className="mt-6 w-10/12">
+            <div className="mt-6 grid grid-cols-2 gap-2">
+                <div>
                     <h2 className="text-lg font-semibold">Jenis Kelamin</h2>
-                    <div className="dropdown mt-2">
-                        <label
-                            tabIndex={0}
-                            className="justify-start w-64 btn text-sm font-semibold bg-white border-[#2570EB] hover:bg-[#2570EB] hover:bg-opacity-20"
-                            onClick={toggleGenderDropdown}
-                        >
-                            <FontAwesomeIcon icon={isGenderDropdownOpen ? faAngleRight : faAngleDown} style={{ marginLeft: '8px' }} />
-                            {selectedGender}
-                        </label>
-                        <ul
-                            tabIndex={0}
-                            className={`dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-64 ${isGenderDropdownOpen ? 'open' : ''}`}
-                        >
-                            <li onClick={() => handleGenderChange("Laki-Laki")}><a>Laki-Laki</a></li>
-                            <li onClick={() => handleGenderChange("Perempuan")}><a>Perempuan</a></li>
-                        </ul>
-                    </div>
+                    <Dropdown
+                        label={selectedGender}
+                        options={genderOptions}
+                        selectedValue={selectedGender}
+                        onValueChange={setSelectedGender}
+                        isOpen={isGenderDropdownOpen}
+                        onToggle={() => setIsGenderDropdownOpen(!isGenderDropdownOpen)}
+                    />
                 </div>
-                <div className="basis-1/2">
+                <div>
                     <h2 className="text-lg font-semibold">Pendidikan</h2>
-                    <div className="dropdown mt-2">
-                        <label
-                            tabIndex={0}
-                            className="justify-start w-80 btn text-sm font-semibold bg-white border-[#2570EB] hover:bg-[#2570EB] hover:bg-opacity-20"
-                            onClick={toggleEducationDropdown}
-                        >
-                            <FontAwesomeIcon icon={isEducationDropdownOpen ? faAngleRight : faAngleDown} style={{ marginLeft: '8px' }} />
-                            {selectedEducation}
-                        </label>
-                        <ul
-                            tabIndex={0}
-                            className={`dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-64 ${isEducationDropdownOpen ? 'open' : ''}`}
-                        >
-                            <li onClick={() => handleEducationChange("Sekolah Dasar")}><a>Sekolah Dasar</a></li>
-                            <li onClick={() => handleEducationChange("Sekolah Menengah Pertama")}><a>Sekolah Menengah Pertama</a></li>
-                            <li onClick={() => handleEducationChange("Sekolah Menengah Atas")}><a>Sekolah Menengah Atas</a></li>
-                        </ul>
-                    </div>
+                    <Dropdown
+                        label={selectedEducation}
+                        options={educationOptions}
+                        selectedValue={selectedEducation}
+                        onValueChange={setSelectedEducation}
+                        isOpen={isEducationDropdownOpen}
+                        onToggle={() => setIsEducationDropdownOpen(!isEducationDropdownOpen)}
+                    />
+                </div>
+                <div className="mt-4">
+                    <h2 className="text-lg font-semibold">Kabupaten</h2>
+                    <Dropdown
+                        label={selectedCity}
+                        options={cityOptions}
+                        selectedValue={selectedCity}
+                        onValueChange={setSelectedCity}
+                        isOpen={isCityDropdownOpen}
+                        onToggle={() => setIsCityDropdownOpen(!isCityDropdownOpen)}
+                    />
+                </div>
+                <div className="mt-4">
+                    <h2 className="text-lg font-semibold">Kecamatan</h2>
+                    <Dropdown
+                        label={selectedSubcity}
+                        options={getSubcityOptions(selectedCity)}
+                        selectedValue={selectedSubcity}
+                        onValueChange={setSelectedSubcity}
+                        isOpen={isSubcityDropdownOpen}
+                        onToggle={() => setIsSubcityDropdownOpen(!isSubcityDropdownOpen)}
+                    />
+                </div>
+                <div className="mt-4">
+                    <h2 className="text-lg font-semibold mb-2 ">Unggah KTP (PDF, Max 3 MB)</h2>
+                    <input type="file" className="file-input file-input-bordered file-input-primary w-[20rem]" />
+                </div>
+                <div className="mt-4">
+                    <h2 className="text-lg font-semibold mb-2 ">Unggah CV (PDF, Max 3 MB)</h2>
+                    <input type="file" className="file-input file-input-bordered file-input-primary w-[20rem]" />
+                </div>
+                <div className="mt-4 col-span-2">
+                    <h2 className="text-lg font-semibold mb-2 ">Detail Halaman</h2>
+                    <input type="text" placeholder="Ceritakan hambatanmu secara singkat" className="input input-bordered input-primary w-[42.5rem] h-32" />
+                </div>
+                <div className="mt-4 col-span-2">
+                    <h2 className="text-lg font-semibold mb-2 ">Minat</h2>
+                    <input type="text" placeholder="Sebutkan minat atau hobimu secara singkat" className="input input-bordered input-primary w-[42.5rem]" />
+                </div>
+                <div className="mt-4 col-span-2">
+                    <h2 className="text-lg font-semibold mb-2 ">Alat Bantu</h2>
+                    <input type="text" placeholder="Tuliskan alat bantu yang biasa kamu gunakan" className="input input-bordered input-primary w-[42.5rem]" />
+                    <p className="mt-1">*Tulis alat bantu yang biasa anda gunakan ketika bekerja</p>
                 </div>
             </div>
         </div>
     );
-};
+}
+
+function getSubcityOptions(city: string) {
+    switch (city) {
+        case "Mataram":
+            return ["Ampenan", "Cakranegara", "Mataram", "Sandubaya", "Sekarbela", "Selaparang"];
+        case "Lombok Timur":
+            return ["Aikmel", "Jerowaru", "Keruak", "Labuhan Haji", "Lenek", "Masbagik", "Montong Gading", "Pringgabaya", "Pringgasela", "Sakra", "Sakra Timur", "Sakra Barat", "Sambelia", "Selong", "Sembalun", "Sikur", "Sukarmulia", "Suralaga", "Suwela", "Terara", "Wanasaba"];
+        case "Lombok Barat":
+            return ["Gerung", "Kediri", "Narmada", "Sekotong", "Labu Api", "Gunung Sari", "Lingsar", "Lembar", "Batu Layar", "Kuripan"];
+        case "Lombok Utara":
+            return ["Bayan", "Gangga", "Kayangan", "Pemenang", "Tanjung"];
+        case "Lombok Tengah":
+            return ["Batukliang", "Batukliang Utara", "Janapria", "Jonggat", "Kopang", "Praya", "Praya Barat", "Praya Barat Daya", "Praya Tengah", "Praya Timur", "Pringgarata", "Pujut"];
+        case "Kabupaten Bima":
+            return ["Ambalawi", "Belo", "Bolo", "Donggo", "Lambitu", "Lambu", "Langgudu", "Mada Pangga", "Monta", "Palibelo", "Parado", "Sanggar", "Sape", "Soromandi", "Tambora", "Wawo", "Wera", "Woha"];
+        case "Dompu":
+            return ["Dompu", "Kempo", "Hu'u", "Kilo", "Woja", "Pekat", "Manggelewa", "Pajo"];
+        case "Sumbawa":
+            return ["Alas", "Alas Barat", "Batu Lanteh", "Buer", "Empang", "Labangka", "Labuan Badas", "Lantung", "Lape", "Lenangguar", "Lunyuk", "Lopok", "Maronge", "Moyo Hilir", "Moyo Hulu", "Moyo Utara", "Orang Telu", "Plampang", "Rhee", "Ropang", "Sumbawa", "Unter Lwes", "Utan", "Tarano"];
+        case "Sumbawa Barat":
+            return ["Jereweh", "Taliwang", "Seteluk", "Sekongkang", "Brang Rea", "Poto Tano", "Brang Ene", "Maluk"];
+        case "Kota Bima":
+            return ["Asakota", "Mpunda", "Raba", "Rasanae Barat", "Rasanae Timur"];
+
+        default:
+            return [];
+    }
+}
