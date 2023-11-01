406d06c7b88f493b573e0e9b800434bfbafaf054
RusdyZain <ryanfajri28@gmail.com>
2023-11-01 09:51:23 +0800

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+const Header = () => {
+  return (
+    <>
+      <div className="flex pt-[9px] justify-between">
+        <div className="w-3/12 flex items-center pl-[102px]">
+          <Image
+            src="/logo_rafflesia.svg"
+            alt="Logo Rafflesia"
+            width={54}
+            height={44}
+          />
+          <h1 className="text-2xl font-bold text-blue-600 pt-2 ">
+            Karir Terbuka
+          </h1>
+        </div>
+        <div className="w-6/12">
+          <ul className="flex justify-center space-x-4 font-semibold">
+            <Link href="/" className="mx-4 my-4">
+              Beranda
+            </Link>
+            <Link href="/" className="mx-4 my-4">
+              Pekerjaan
+            </Link>
+            <Link href="/" className="mx-4 my-4">
+              Komunitas
+            </Link>
+            <Link href="/" className="mx-4 my-4">
+              Blog
+            </Link>
+            <Link href="/" className="mx-4 my-4">
+              Tentang
+            </Link>
+          </ul>
+        </div>
+        <div className="pr-[106px] ">
+          <Image
+            src="/avatar_users.svg"
+            alt="Logo Rafflesia"
+            width={40}
+            height={40}
+            className="rounded-full pt-4 "
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
